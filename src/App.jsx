cfb90e2b3f73b04805f315b8a0103457e90528d2
’Marina <148355147+MaRos10@users.noreply.github.com>
2024-10-16 14:56:00 +0200

    Added animation to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./App.css";
 import "./Mediaqueries.css";
 import Nav from "./components/Nav/Nav";
@@ -10,16 +11,78 @@ import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
+const animationSettings = {
+  whileInView: { opacity: 1, y: 0, scale: 1 },
+  initial: { opacity: 0, y: 30, scale: 0.95 },
+  transition: { duration: 0.7, ease: "easeOut" },
+};
+
 function App() {
   return (
     <div className="App">
       <Nav />
       <HamburgerNav />
-      <Profile />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
+
+      <motion.div {...animationSettings}>
+        <Profile />
+      </motion.div>
+
+      <motion.div {...animationSettings}>
+        <About />
+      </motion.div>
+
+      <motion.div {...animationSettings}>
+        <Experience />
+      </motion.div>
+
+      <motion.div {...animationSettings}>
+        <Projects />
+      </motion.div>
+
+      <motion.div {...animationSettings}>
+        <Contact />
+      </motion.div>
+
+      {/*       <motion.section
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        initial={{ opacity: 0, y: 30, scale: 0.95 }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+      >
+        <Profile />
+      </motion.section>
+
+      <motion.div
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        initial={{ opacity: 0, y: 30, scale: 0.95 }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+      >
+        <About />
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        initial={{ opacity: 0, y: 30, scale: 0.95 }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+      >
+        <Experience />
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        initial={{ opacity: 0, y: 30, scale: 0.95 }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+      >
+        <Projects />
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        initial={{ opacity: 0, y: 30, scale: 0.95 }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+      >
+        <Contact />
+      </motion.div> */}
+
       <Footer />
     </div>
   );
